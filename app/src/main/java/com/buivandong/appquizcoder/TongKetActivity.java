8d6d2ef49793edcde8d9d0a5567d769485956691
Van Dong <vandong78py@gmail.com>
2024-05-18 21:48:28 +0700

    update layout QuestionActivity and code logic question

---
+++ app/src/main/java/com/buivandong/appquizcoder/TongKetActivity.java
@@ -0,0 +1,14 @@
+package com.buivandong.appquizcoder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TongKetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tong_ket);
+    }
+}
--- app/src/main/java/com/buivandong/appquizcoder/TopicFragment.java
+++ app/src/main/java/com/buivandong/appquizcoder/TopicFragment.java
@@ -1,7 +1,9 @@
 package com.buivandong.appquizcoder;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -12,7 +14,24 @@ public class TopicFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_topic, container, false);
+        // Inflate the correct layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_topic, container, false);
+
+        // Find your CardView elements here
+        CardView reactCard = view.findViewById(R.id.react_card);
+        CardView javascriptCard = view.findViewById(R.id.javascript_card);
+        CardView htmlCard = view.findViewById(R.id.html_card);
+        CardView pythonCard = view.findViewById(R.id.python_card);
+        CardView cplusCard = view.findViewById(R.id.cplus_card);
+        CardView javaCard = view.findViewById(R.id.java_card);
+
+        reactCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(requireActivity(), QuestionsReactJS.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
