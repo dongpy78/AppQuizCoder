81d9993744c3e6d978b37e8faa9b3482fb746ec8
Van Dong <vandong78py@gmail.com>
2024-05-18 21:09:45 +0700

    update card topic

--- app/src/main/java/com/buivandong/appquizcoder/StartFragment.java
+++ app/src/main/java/com/buivandong/appquizcoder/StartFragment.java
@@ -3,10 +3,13 @@ package com.buivandong.appquizcoder;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class StartFragment extends Fragment {
@@ -14,6 +17,31 @@ public class StartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false);
+        View view = inflater.inflate(R.layout.fragment_start, container, false);
+        Button chuyen_man_hinh = view.findViewById(R.id.button_start);
+        chuyen_man_hinh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create an instance of the TopicFragment
+                TopicFragment topicFragment = new TopicFragment();
+
+                // Get the FragmentManager
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+
+                // Begin a FragmentTransaction
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+                // Replace the current fragment with the TopicFragment
+                transaction.replace(R.id.fragment_container, topicFragment);
+
+                // Add the transaction to the back stack
+                transaction.addToBackStack(null);
+
+                // Commit the transaction
+                transaction.commit();
+            }
+        });
+
+        return view;
     }
 }
